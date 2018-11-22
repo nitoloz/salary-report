4727cc3a98b9c6611365edfae5437d18ee3cc7f3
Andrey Zolotin <CSTX_a_AZOLOTI@corpdir.onmicrosoft.com>
2018-11-22 11:48:45 +0100

    Add x and y axis

--- scatter-plot.js
+++ scatter-plot.js
@@ -1,10 +1,12 @@
+const margin = {top: 50, right: 50, bottom: 50, left: 50};
+const height = 500;
 const width = 1000;
-const height = 600;
 
 let svg = d3.select("#chart-area")
     .append('svg')
+    .attr('height', height)
     .attr('width', width)
-    .attr('height', height);
+// .attr('transform','translate(' + margin.left + ',' + margin.top + ')');
 
 
 d3.csv('data/salaries-responses.csv')
@@ -59,14 +61,25 @@ d3.csv('data/salaries-responses.csv')
             .domain([
                 d3.min([0, d3.min(experience)]),
                 d3.max([0, d3.max(experience)])
-            ]).range([0, width]);
+            ]).range([margin.left, width - margin.right]);
 
         let yScale = d3.scaleLog()
             .domain([
                 d3.min([d3.min(salaries)]),
                 d3.max([d3.max(salaries)])
-            ]).range([height, 0])
-            .base(0.1);
+            ]).range([height - margin.top, margin.bottom])
+            .base(10);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + (height - margin.top) + ")")
+            .call(d3.axisBottom(xScale));
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + margin.left + ",0)")
+            .call(d3.axisLeft(yScale));
+
         // let yScale = d3.scaleLinear()
         //     .domain([
         //         d3.min([0, d3.min(salaries)]),
@@ -108,26 +121,25 @@ d3.csv('data/salaries-responses.csv')
             .attr('fill', function (d, i) {
                 return colorScale(d[SEX])
             })
-        // .on('mouseover', function () {
-        //     d3.select(this)
-        //         .transition()
-        //         .duration(500)
-        //         .attr('r', 20)
-        //         .attr('stroke-width', 3)
-        // })
-        // .on('mouseout', function () {
-        //     d3.select(this)
-        //         .transition()
-        //         .duration(500)
-        //         .attr('r', 10)
-        //         .attr('stroke-width', 1)
-        // })
-        // .append('title') // Tooltip
-        // .text(function (d) {
-        //     return d.variable +
-        //         '\nReturn: ' + formatPercent(d.aror) +
-        //         '\nStd. Dev.: ' + formatPercent(d.asd)
-        // })
+            .on('mouseover', function () {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .attr('r', 10)
+                    .attr('stroke-width', 3)
+            })
+            .on('mouseout', function () {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .attr('r', 5)
+                    .attr('stroke-width', 1)
+            })
+            .append('title')
+            .text(function (d) {
+                return 'Total Experience: ' + d[TOTAL_EXPERIENCE] +
+                    '\nSalary: ' + d[CURRENT_SALARY]
+            })
     });
 
 
