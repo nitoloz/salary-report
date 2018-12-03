932d66e9b0fbc23483a67e20a2457cfe2f10a93e
nitoloz <andrey.zolotin@gmail.com>
2018-12-03 23:28:25 +0100

    Add pie chart with population grouped by sex Add circle with info in the center of pie chart

--- pie-chart.js
+++ pie-chart.js
@@ -1,19 +1,40 @@
 let cityPieChart = pieChart()
     .width(width / 1.5)
     .height(height / 1.5)
-    .groupByOption(CITY);
+    .groupByOption(CITY)
+    .groupByOptionLabel('City')
+    .valueLabel('Number of respondents');
+
+let sexColorScale = d3.scaleOrdinal() // D3 Version 4
+    .domain(["M", "F"])
+    .range(["#80b1d3", "#fb8072"]);
+
+let sexPieChart = pieChart()
+    .width(width / 1.5)
+    .height(height / 1.5)
+    .groupByOption(SEX)
+    .groupByOptionLabel('Sex')
+    .valueLabel('Number of respondents')
+    .colorScale(sexColorScale);
 
 d3.csv('data/salaries-responses.csv')
     .then((data) => {
         d3.select("#city-pie-chart-area")
             .datum(data)
             .call(cityPieChart);
+
+        d3.select("#sex-pie-chart-area")
+            .datum(data)
+            .call(sexPieChart);
     });
 
 function pieChart() {
     let width = 1000,
         height = 600,
-        groupByOption = CITY;
+        groupByOption = CITY,
+        groupByOptionLabel = 'City',
+        valueLabel = 'Number of respondents',
+        colorScale = d3.scaleOrdinal(d3.schemePastel2);
 
     function chart(selection) {
         selection.each(function (data) {
@@ -36,11 +57,10 @@ function pieChart() {
                 .entries(data);
 
             let radius = Math.min(width, height) / 2;
-            let color = d3.scaleOrdinal(d3.schemePastel2);
 
             let arc = d3.arc()
-                .innerRadius(0.5 * radius)
-                .outerRadius(radius)
+                .innerRadius(radius * 0.5)
+                .outerRadius(radius * 0.9)
                 .cornerRadius(8);
             // .padAngle(0.01);
 
@@ -52,39 +72,32 @@ function pieChart() {
                     return a.value - b.value;
                 });
 
-            let tooltip = d3.tip()
-                .attr("class", "d3-tip")
-                // .offset([-8, 0])
-                .html(function (d) {
-                    return `City: ${d.key}<br>
-                    Number of respondents: ${d.value}`
-                });
-
-            linePlotSvg.call(tooltip);
-
             let path = pieChartSvg.selectAll('path')
                 .data(pie(groupedData))
                 .enter()
                 .append("g")
                 .append('path')
                 .attr('d', arc)
-                .attr('fill', (d, i) => color(i))
+                .attr('fill', (d, i) => colorScale(i))
                 .style('stroke', 'white')
-                .on("mouseover", function (d) {
-                    // d3.select(this)
-                    //     .transition()
-                    //     .duration(100)
-                    //     .attr('r', 10)
-                    //     .attr('stroke-width', 3);
-                    tooltip.show(d.data);
+                .on("mouseover", function (d, i) {
+                    pieChartSvg.append('text')
+                        .attr('class', 'toolCircle')
+                        .attr('dy', -15) // hard-coded. can adjust this to adjust text vertical alignment in tooltip
+                        .html(function () {
+                            return `${d.data.key}: ${d.data.value}`
+                        })
+                        .style('font-size', '.9em')
+                        .style('text-anchor', 'middle'); // centres text in tooltip
+
+                    pieChartSvg.append('circle')
+                        .attr('class', 'toolCircle')
+                        .attr('r', radius * 0.45) // radius of tooltip circle
+                        .style('fill', colorScale(i)) // colour based on category mouse is over
+                        .style('fill-opacity', 0.35);
                 })
                 .on("mouseout", function () {
-                    // d3.select(this)
-                    //     .transition()
-                    //     .duration(100)
-                    //     .attr('r', 5)
-                    //     .attr('stroke-width', 1);
-                    tooltip.hide();
+                    d3.selectAll('.toolCircle').remove();
                 });
         })
     }
@@ -104,6 +117,24 @@ function pieChart() {
     chart.groupByOption = function (value) {
         if (!arguments.length) return groupByOption;
         groupByOption = value;
+        return chart;
+    };
+
+    chart.groupByOptionLabel = function (value) {
+        if (!arguments.length) return groupByOptionLabel;
+        groupByOptionLabel = value;
+        return chart;
+    };
+
+    chart.valueLabel = function (value) {
+        if (!arguments.length) return valueLabel;
+        valueLabel = value;
+        return chart;
+    };
+
+    chart.colorScale = function (value) {
+        if (!arguments.length) return colorScale;
+        colorScale = value;
         return chart;
     };
 
