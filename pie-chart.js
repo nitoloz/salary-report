9f64865dfc4565d6735183ce6929717f00490c29
Andrey_Zolotin <andrey.zolotin@gmail.com>
2018-12-11 13:24:56 +0100

    feat(pie): add grouping by company type, language, size

--- pie-chart.js
+++ pie-chart.js
@@ -9,40 +9,60 @@ d3.csv('data/salaries-responses.csv')
             .domain(["M", "F"])
             .range(["#80b1d3", "#fb8072"]);
 
-        d3.selectAll("input")
-            .on("change", function () {
-                switch (this.value) {
-                    case 'City':
-                        dynamicPieChart
-                            .groupByOptionLabel('City')
-                            .colorScale(d3.scaleOrdinal(d3.schemeSet3))
-                            .data(groupedDataCity);
-                        break;
-                    case 'Sex':
-                        dynamicPieChart
-                            .groupByOptionLabel('Sex')
-                            .colorScale(sexColorScale)
-                            .data(groupedDataSex);
-                        break;
-                    case 'Seniority':
-                        dynamicPieChart
-                            .groupByOptionLabel('Seniority')
-                            .colorScale(d3.scaleOrdinal(d3.schemeSet3))
-                            .data(groupedDataSeniority);
-                        break;
-                    default:
-                        break;
-                }
-            });
+        document.querySelector('select[id="pieChartSelect"]').onchange = function (event) {
+            switch (event.target.value) {
+                case 'City':
+                    dynamicPieChart
+                        .groupByOptionLabel('City')
+                        .colorScale(d3.scaleOrdinal(d3.schemeSet3))
+                        .data(dataGroupedByCity);
+                    break;
+                case 'Sex':
+                    dynamicPieChart
+                        .groupByOptionLabel('Sex')
+                        .colorScale(sexColorScale)
+                        .data(dataGroupedBySex);
+                    break;
+                case 'Seniority':
+                    dynamicPieChart
+                        .groupByOptionLabel('Seniority')
+                        .colorScale(d3.scaleOrdinal(d3.schemeSet3))
+                        .data(dataGroupedBySeniority);
+                    break;
+                case 'CompanyType':
+                    dynamicPieChart
+                        .groupByOptionLabel('Type')
+                        .colorScale(d3.scaleOrdinal(d3.schemeSet3))
+                        .data(dataGroupedByCompanyType);
+                    break;
+                case 'Language':
+                    dynamicPieChart
+                        .groupByOptionLabel('Language')
+                        .colorScale(d3.scaleOrdinal(d3.schemeSet3))
+                        .data(dataGroupedByLanguage);
+                    break;
+                case 'Size':
+                    dynamicPieChart
+                        .groupByOptionLabel('Size')
+                        .colorScale(d3.scaleOrdinal(d3.schemeSet3))
+                        .data(dataGroupedByCompanySize);
+                    break;
+                default:
+                    break;
+            }
+        };
 
-        let groupedDataCity = processPieChartData(data, CITY);
-        let groupedDataSex = processPieChartData(data, SEX);
-        let groupedDataSeniority = processPieChartData(data, SENIORITY_LEVEL);
+        let dataGroupedByCity = processPieChartData(data, CITY);
+        let dataGroupedBySex = processPieChartData(data, SEX);
+        let dataGroupedBySeniority = processPieChartData(data, SENIORITY_LEVEL);
+        let dataGroupedByCompanyType = processPieChartData(data, COMPANY_TYPE);
+        let dataGroupedByLanguage = processPieChartData(data, WORK_LANGUAGE);
+        let dataGroupedByCompanySize = processPieChartData(data, COMPANY_SIZE);
 
         dynamicPieChart
             .groupByOptionLabel('City')
             .colorScale(d3.scaleOrdinal(d3.schemeSet3))
-            .data(groupedDataCity);
+            .data(dataGroupedByCity);
 
         d3.select("#pie-chart-area")
             .call(dynamicPieChart);
