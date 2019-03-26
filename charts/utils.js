e2420c3924342b1504b75c664a2a7c12efb84b97
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-03-26 08:18:04 +0100

    feat: add simple side panel

--- charts/utils.js
+++ charts/utils.js
@@ -64,3 +64,12 @@ class Utils {
             .text(text);
     }
 }
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
