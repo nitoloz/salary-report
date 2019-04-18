4e1f0cb62a7425ec574f2797c72f5c825117628a
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-04-18 17:43:07 +0200

    feat(filters): add sex filters to panel

--- filters/filters.js
+++ filters/filters.js
@@ -1,26 +1,47 @@
 class Filters {
 
     constructor() {
+        this.filters = {
+            sex: {
+                areaId: 'sex-filters',
+                label: 'Sex',
+                values: []
+            },
+            city: {
+                areaId: 'city-filters',
+                label: 'City',
+                values: []
+            },
+            companyType: {
+                areaId: 'company-type-filters',
+                label: 'Company type',
+                values: []
+            },
+            seniorityLevel: {
+                areaId: 'seniority-level-filters',
+                label: 'Seniority level',
+                values: []
+            }
+        };
     }
 
     updateData(data) {
         this.data = data;
-        this.filters = [];
         this.calculateFilterValues();
         this.appendFiltersToPage();
     }
 
     calculateFilterValues() {
-        this.cities = [...new Set(this.data.map(d => d[DataProperties.CITY]).sort())];
-        this.genders = [...new Set(this.data.map(d => d[DataProperties.SEX]).sort())];
-        this.companyTypes = [...new Set(this.data.map(d => d[DataProperties.COMPANY_TYPE]).sort())];
-        this.seniorityLevels = [...new Set(this.data.map(d => d[DataProperties.SENIORITY_LEVEL]).sort())];
+        this.filters.city.values = [...new Set(this.data.map(d => d[DataProperties.CITY]).sort())];
+        this.filters.sex.values = [...new Set(this.data.map(d => d[DataProperties.SEX]).sort())];
+        this.filters.companyType.values = [...new Set(this.data.map(d => d[DataProperties.COMPANY_TYPE]).sort())];
+        this.filters.seniorityLevel.values = [...new Set(this.data.map(d => d[DataProperties.SENIORITY_LEVEL]).sort())];
     }
 
     appendFiltersToPage() {
-        const sexFiltersArea = document.getElementById('sex-filters');
-        sexFiltersArea.innerHTML = '';
-        this.genders.forEach((sex, index) => {
+        const sexFiltersArea = document.getElementById(this.filters.sex.areaId);
+        sexFiltersArea.innerHTML = '<hr><h5 class="text-center">Sex</h5>';
+        this.filters.sex.values.forEach((sex, index) => {
             this.appendCheckbox(sexFiltersArea, sex, index)
         });
     }
@@ -28,7 +49,7 @@ class Filters {
     appendCheckbox(filterArea, value, index) {
         let input = document.createElement("input");
         input.value = (value + '</br>');
-        input.checked  = true;
+        input.checked = true;
         input.type = "checkbox";
         input.id = "color" + index;
         let text = document.createElement("span");
@@ -37,5 +58,6 @@ class Filters {
         filterArea.appendChild(text);
         filterArea.appendChild(input);
         filterArea.appendChild(br);
-    };
+    }
+    ;
 }
