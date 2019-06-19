31160b70b10788edbd8a06f168eb3e70760be45e
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-06-19 07:58:36 +0200

    feat(filter): add reset button for range slider

--- filters/range-filter.js
+++ filters/range-filter.js
@@ -22,11 +22,11 @@ class RangeFilter extends BaseFilter {
             }
         };
 
-        const selectAllButton = document.createElement("button");
-        selectAllButton.innerHTML = "Select all";
-        selectAllButton.id = that.areaId + '_select_all';
-        selectAllButton.className = 'btn btn-outline-secondary btn-sm';
-        selectAllButton.onclick = function (event) {
+        const resetButton = document.createElement("button");
+        resetButton.innerHTML = "Reset filter";
+        resetButton.id = that.areaId + '_reset';
+        resetButton.className = 'btn btn-outline-secondary btn-sm';
+        resetButton.onclick = function (event) {
             // that.selectedValues = that.values.map(value => value.value).slice();
             // const checkBoxes = filterArea.getElementsByTagName('input');
             // for (let checkBox of checkBoxes) {
@@ -35,62 +35,46 @@ class RangeFilter extends BaseFilter {
             document.dispatchEvent(that.event);
         };
 
-        const deselectAllButton = document.createElement("button");
-        deselectAllButton.innerHTML = "Deselect all";
-        deselectAllButton.id = that.areaId + '_deselect_all';
-        deselectAllButton.className = 'btn btn-outline-secondary btn-sm';
-        deselectAllButton.onclick = function (event) {
-            // that.selectedValues = [];
-            // const checkBoxes = filterArea.getElementsByTagName('input');
-            // for (let checkBox of checkBoxes) {
-            //     checkBox.checked = false
-            // }
-            document.dispatchEvent(that.event);
-        };
-
         const buttonGroup = document.createElement("div");
         buttonGroup.role = 'group';
         buttonGroup.className = 'btn-group btn-group-justified';
-        buttonGroup.appendChild(selectAllButton);
-        buttonGroup.appendChild(deselectAllButton);
+        buttonGroup.appendChild(resetButton);
 
         const filterGroup = document.createElement("div");
         filterGroup.className = 'collapse show';
         filterGroup.id = this.areaId + '-filters-group';
         filterGroup.appendChild(buttonGroup);
-        // this.values.forEach((value, index) => {
-        //     this.appendCheckbox(filterGroup, value, index)
-        // });
+        this.appendSlider(filterGroup, this.values);
         filterArea.appendChild(filterGroup);
     }
 
 
-    appendCheckbox(filterArea, value, index) {
-        let input = document.createElement("input");
-        input.value = (value + '</br>');
-        input.checked = true;
-        input.type = "checkbox";
-        input.id = this.areaId + index;
-        const that = this;
-        input.addEventListener('change', function () {
-            if (this.checked) {
-                that.selectedValues.push(value.value);
-            } else {
-                let selectedItemIndex = that.selectedValues.indexOf(value.value);
-                if (selectedItemIndex !== -1) {
-                    that.selectedValues.splice(selectedItemIndex, 1);
-                }
-            }
-            document.dispatchEvent(that.event);
-        });
-
-        let text = document.createElement("span");
-        text.innerHTML = `${value.value} (${value.count})`;
-        let br = document.createElement("br");
-
-        filterArea.appendChild(input);
-        filterArea.appendChild(text);
-        filterArea.appendChild(br);
+    appendSlider(filterArea, values) {
+        // let input = document.createElement("input");
+        // input.value = (value + '</br>');
+        // input.checked = true;
+        // input.type = "checkbox";
+        // input.id = this.areaId + index;
+        // const that = this;
+        // input.addEventListener('change', function () {
+        //     if (this.checked) {
+        //         that.selectedValues.push(value.value);
+        //     } else {
+        //         let selectedItemIndex = that.selectedValues.indexOf(value.value);
+        //         if (selectedItemIndex !== -1) {
+        //             that.selectedValues.splice(selectedItemIndex, 1);
+        //         }
+        //     }
+        //     document.dispatchEvent(that.event);
+        // });
+        //
+        // let text = document.createElement("span");
+        // text.innerHTML = `${value.value} (${value.count})`;
+        // let br = document.createElement("br");
+        //
+        // filterArea.appendChild(input);
+        // filterArea.appendChild(text);
+        // filterArea.appendChild(br);
     }
 
     isFilterSelected() {
