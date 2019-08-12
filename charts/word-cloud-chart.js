705b6f1dcfb04e6c7f97e5ca707ccf87ff81c422
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-08-12 16:27:06 +0200

    feat(cloud): add chart drawing function

--- charts/word-cloud-chart.js
+++ charts/word-cloud-chart.js
@@ -14,7 +14,55 @@ function wordCloudChartD3(){
 
     function chart(selection) {
         selection.each(function () {
+            var myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
 
+// // set the dimensions and margins of the graph
+//             var margin = {top: 10, right: 10, bottom: 10, left: 10},
+//                 width = 450 - margin.left - margin.right,
+//                 height = 450 - margin.top - margin.bottom;
+//
+// // append the svg object to the body of the page
+//             var svg = d3.select(this)
+//                 .attr("width", width + margin.left + margin.right)
+//                 .attr("height", height + margin.top + margin.bottom)
+//                 .append("g")
+//                 .attr("transform",
+//                     "translate(" + margin.left + "," + margin.top + ")");
+
+            const svg = selection
+                .append('svg')
+                .attr('height', height)
+                .attr('width', width);
+
+// Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+// Wordcloud features that are different from one word to the other must be here
+            var layout = d3.layout.cloud()
+                .size([width, height])
+                .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+                .padding(5)        //space between words
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .fontSize(function(d) { return d.size; })      // font size of words
+                .on("end", draw);
+            layout.start();
+
+// This function takes the output of 'layout' above and draw the words
+// Wordcloud features that are THE SAME from one word to the other can be here
+            function draw(words) {
+                svg
+                    .append("g")
+                    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                    .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function(d) { return d.size; })
+                    .style("fill", "#69b3a2")
+                    .attr("text-anchor", "middle")
+                    .style("font-family", "Impact")
+                    .attr("transform", function(d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.text; });
+            }
 
             updateData = function () {
 
